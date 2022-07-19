449d929b6f8ece3b2fb397e161edd8bfc30b74a6
igor ujiie <igorkazuhico@hotmail.com>
2022-07-19 19:39:34 -0300

    aula 1 inicial

---
+++ src/App.java
@@ -0,0 +1,38 @@
+import static java.net.http.HttpRequest.newBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        //fazer uma conecçao http com imdb e buscar o top 250
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+
+        //pegar só os dados de interesse(titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>>listaDeFilmes = parser.parse(body);
+        
+        //exibir e manipular os dados da maneira que quiser
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+
+    }
+}
